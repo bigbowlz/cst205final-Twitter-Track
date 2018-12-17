dd3b97caa8df6a146ff94ba9189401bc1413b2c2
Wanli Zhou <wzhou@csumb.edu>
2018-12-16 17:12:31 -0800

    added a step that clears all the pngs in the static folder before the query is submitted, fixed the Tweets page so now without a query submission users will see redirection instruction, but the same thing failed for the Sentiment page

--- final_project.py
+++ final_project.py
@@ -2,13 +2,13 @@ from datetime import datetime
 from flask import Flask, render_template, url_for, redirect
 from form import UserInput
 from sentiment_analysis import SentimentAnalysis
+import os
+
 
 analysis = []
 tweets = []
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-
-
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,6 +20,13 @@ def index():
     form = UserInput()
     
     if form.validate_on_submit():
+        #remove existing pngs
+        dir = "static"
+        files = os.listdir(dir)
+        for file in files:
+            if file.endswith(".png"):
+                os.remove(os.path.join(dir,file))
+            
         #gets data from the form
         keyword = form.keyword.data
         numTweets = int(form.numTweets.data)
@@ -36,8 +43,9 @@ def index():
 
 @app.route('/sentiment')
 def sentiment():
-    
-    return render_template("sentiment.html", title = "Sentiments", analysis = analysis, keyword = f'sentiment_{keyword}.png')
+    #if (len(analysis) > 0) == False:
+        #keyword = 0        
+    return render_template("sentiment.html", title = "Sentiments", analysis = analysis, file = f'sentiment_{keyword}.png')
 
 @app.route('/tweets')
 def tweets():
